63741be6595fcf4ca8e95b178e3f0b124e67f21d
subhash singh <subhashsingh1236@gmail.com>
2024-05-24 22:40:39 +0530

    subhash

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import foodRouter from "./routes/foodRouter.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
+import path from 'path'
 
 // app config
 const app = express()
@@ -19,12 +20,15 @@ app.use(cors())
 // db connection
 connectDB()
 
+
 // api endpoints
 app.use("/api/user", userRouter)
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/cart", cartRouter)
 app.use("/api/order",orderRouter)
+
+
 
 app.get("/", (req, res) => {
     res.send("API Working")
