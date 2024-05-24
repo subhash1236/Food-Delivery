9cdb13f56d02c7b1f9302b25b122691a0105d7af
subhash singh <subhashsingh1236@gmail.com>
2024-05-24 17:14:27 +0530

    first commit

---
+++ routes/orderRoute.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import authMiddlewarw from "../middleware/auth.js"
+import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from '../controllers/orderController.js'
+import authMiddleware from '../middleware/auth.js';
+
+const orderRouter = express.Router();
+
+orderRouter.post("/place",authMiddlewarw,placeOrder);
+orderRouter.post("/verify",verifyOrder);
+orderRouter.post("/userorders",authMiddleware,userOrders);
+orderRouter.get("/list",listOrders);
+orderRouter.post("/status",updateStatus);
+
+
+export default orderRouter;
